327746fc249b2a6afab4e1c74dbfbaa2c4d9ab8a
Abraham <abrahamchalita@hotmail.com>
2022-03-14 21:50:34 -0600

    Created home page sections, and added primary functionality of header

---
+++ javascript/script.js
@@ -0,0 +1,3 @@
+function openVideo() {
+    document.getElementById("trailer").innerHTML = "<video style='position:absolute;height:620px;top:215px;left:400px' controls> <source src='video/cuphead.mp4 type='video/mp4'> <source src='video/cuphead.mp4' type=''video/ogg'>Your browser does not support the video tag.</video>"
+}
